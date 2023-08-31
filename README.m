6b3d6f895b3852d41357e7a98633728b845664a2
Kwroo <143759610+Kwroo@users.noreply.github.com>
2023-08-31 19:35:06 -0300

    KARO!
    Lê aí se você quiser Kkkk

---
+++ README.m
@@ -0,0 +1,7 @@
+Hello! i'm Karo! If you want to know me better, here is some information about me:
+I'm bilingual, I speak English and Portuguese (I'm Brazilian) but I can speak a little Spanish
+i like Ben drowned ARG, slenderverse, creepypasta,qsmp and dsmp, i love them so if you know any of them know that we will be good friends!
+I like to draw, so if you want to see my art follow me on Tumblr! if you want of course,(@Kwroo) this is my tumblr 
+my pronouns are he/she 👍
+I'm clearly going to enjoy talking to you! I hope you like me :)
+that's it! 👍
